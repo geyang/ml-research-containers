99d688e22776bcd2f85dd6e337a02bb65956f020
Timur Garipov <timgaripov@gmail.com>
2022-10-23 17:29:05 -0400

    add diffuser image ubuntu20.04 + test

---
+++ offline-rl/diffuser/test.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+
+    print('Testing imports')
+
+    import numpy as np
+    import torch
+    import torchvision
+    import jax
+    import jax.numpy as jnp
+    import flax
+
+    import gym
+    import d4rl
+    import mujoco_py
+    import dm_control
+
+    import ml_logger, jaynes, params_proto
+
+    print('Done')
