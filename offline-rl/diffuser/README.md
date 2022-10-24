b91f91b3cac8722ef67718cf8188db9fcd978c0f
Ge Yang <yangge1987@gmail.com>
2022-10-23 20:09:32 -0400

    got all dmc to run as well

---
+++ offline-rl/diffuser/README.md
@@ -0,0 +1,4 @@
+# Docker Image for D4RL
+
+Done
+- [x] need to fix the test script. to use docker mount.
--- offline-rl/diffuser/test.py
+++ offline-rl/diffuser/test.py
@@ -1,6 +1,28 @@
-if __name__ == '__main__':
+def gym_render():
+    import gym
+    from ml_logger import logger
 
+    env = gym.make("Reacher-v2")
+    logger.print("gym Reacher-v2 is working!", color="green")
+    img = env.render('rgb_array')
+    logger.print(f"Reacher-v2 renders <{img.shape}>", color="green")
+
+
+def gym_dmc_render():
+    import gym
+    from ml_logger import logger
+
+    env = gym.make("dmc:Cartpole-balance-v1")
+    logger.print(f"dmc Cartpole starts!", color="green")
+    img = env.render('rgb_array')
+    logger.print(f"dmc:Cartpole renders <{img.shape}>", color="green")
+
+
+if __name__ == '__main__':
     print('Testing imports')
+    import os
+
+    print(">>>> working directory:", os.getcwd())
 
     import numpy as np
     import torch
@@ -15,5 +37,8 @@ if __name__ == '__main__':
     import dm_control
 
     import ml_logger, jaynes, params_proto
+
+    gym_dmc_render()
+    gym_render()
 
     print('Done')
